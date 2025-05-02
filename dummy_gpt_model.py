65fff4e71ebed566f3a6890c8ca2d8e53c20de2e
Seokhyun0079 <seokhyun0324@gmail.com>
2025-05-03 01:04:08 +0900

    2025/05/03

--- dummy_gpt_model.py
+++ dummy_gpt_model.py
@@ -1,6 +1,16 @@
 import torch
 import torch.nn as nn
 
+GPT_CONFIG_124M = {
+  "vocab_size": 50257,
+  "context_length": 1024,
+  "emb_dim": 768,
+  "n_heads": 12,
+  "n_layers" : 12,
+  "drop_rate" : 0.1,
+  "qkv_bias" : False
+}
+
 class DummyGPTModel(nn.Module):
   def __init__(self, cfg):
     super().__init__()
@@ -12,7 +22,7 @@ class DummyGPTModel(nn.Module):
       *[DummyTransformerBlock(cfg) for _ in range(cfg['n_layers'])],
     )
     self.final_norm = DummyLayerNorm(cfg['emb_dim'])
-    self.out_head == nn.Linear(
+    self.out_head = nn.Linear(
       cfg["emb_dim"], cfg["vocab_size"], bias=False
     )
   
@@ -37,3 +47,30 @@ class DummyLayerNorm(nn.Module):
     super().__init__()
   def forward(self, x):
     return x
+
+import tiktoken
+
+tokeninzer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokeninzer.encode(txt1)))
+batch.append(torch.tensor(tokeninzer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
+
+
+torch.manual_seed(123)
+batch_example = torch.randn(2,5)
+layer = nn.Sequential()
+out = layer(batch_example)
+print(out)
+
