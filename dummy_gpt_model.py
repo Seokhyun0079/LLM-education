10f0a71483074247f5462ec56ea75ceccfb15ff3
Seokhyun0079 <seokhyun0324@gmail.com>
2025-07-08 00:04:44 +0900

    2025/07/08

--- dummy_gpt_model.py
+++ dummy_gpt_model.py
@@ -70,7 +70,42 @@ print(logits)
 
 torch.manual_seed(123)
 batch_example = torch.randn(2,5)
-layer = nn.Sequential()
+print("batch_example:\n", batch_example)
+layer = nn.Sequential(nn.Linear(5,6), nn.ReLU())
 out = layer(batch_example)
-print(out)
+print("out:\n", out)
+
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+print("out-mean:\n", out-mean)
+print("torch.sqrt(var):\n", torch.sqrt(var))
+out_norm = (out - mean) / torch.sqrt(var)
+print("Normalized layer outputs:\n", out_norm)
+
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+
+class LayerNorm(nn.Module):
+  def __iinit__(self, emb_dim):
+    super().__init__()
+    self.eps = 1e-5
+    self.scale = nn.Parameter(torch.ones(emb_dim))
+    self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+  def forward(self, x):
+    mean = x.mean(dim=-1, keepdim=True)
+    var = x.var(dim=-1, keepdim=True, unbiased=False)
+    norm_x = (x - mean) / torch.sqrt(var + self.eps)
+    return self.scale * norm_x + self.shift
 
